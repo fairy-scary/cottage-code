ad3f857407d5d1abb7b18b875944e5d6ef3279f2
fairy-scary <ecoautumn@gmail.com>
2021-02-17 10:28:09 -0600

    heroku scripts

--- express/routes/index.js
+++ express/routes/index.js
@@ -5,5 +5,33 @@ router.get(/^(?!\/?api).*/, (req, res) => {
   res.send('Hello World!');
 });;
 
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
+// Static routes
+// Serve React build files in production
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  // Serve the frontend's index.html file at the root route
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+
+  // Serve the static assets in the frontend's build folder
+  router.use(express.static(path.resolve("../frontend/build")));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+}
+
 
 module.exports = router;
