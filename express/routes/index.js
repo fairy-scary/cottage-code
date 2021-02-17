0d8a7e6179298d61fe655699472d158750232931
fairy-scary <ecoautumn@gmail.com>
2021-02-17 12:39:37 -0600

    bug fixg

--- express/routes/index.js
+++ express/routes/index.js
@@ -1,34 +1,34 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const apiRouter = require('./api');
 
-router.get(/^(?!\/?api).*/, (req, res) => {
-  res.send('Hello World!');
-});;
+router.use('/api', apiRouter);
 
+// Serve the static assets in the frontend's build folder
+router.use(express.static(path.resolve("../frontend/build")));
 
-// // Static routes
-// // Serve React build files in production
-// if (process.env.NODE_ENV === 'production') {
-//   const path = require('path');
-//   // Serve the frontend's index.html file at the root route
-//   router.get('/', (req, res) => {
-//     res.cookie('XSRF-TOKEN', req.csrfToken());
-//     res.sendFile(
-//       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
-//     );
-//   });
+// Static routes
+// Serve React build files in production
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  // Serve the frontend's index.html file at the root route
+  router.get('/', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'public', 'index.html')
+    );
+  });
 
-//   // Serve the static assets in the frontend's build folder
-//   router.use(express.static(path.resolve("../frontend/build")));
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'public', 'index.html')
+    );
+  });
+}
 
-//   // Serve the frontend's index.html file at all other routes NOT starting with /api
-//   router.get(/^(?!\/?api).*/, (req, res) => {
-//     res.cookie('XSRF-TOKEN', req.csrfToken());
-//     res.sendFile(
-//       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
-//     );
-//   });
-// }
-
+//Server Test Route
+// router.get(/^(?!\/?api).*/, (req, res) => {
+//   res.send('Hello World!');
+// });;
 
 module.exports = router;
